542151185793777bd0dcf0de1ae18d29b90ce651
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-02-01 16:49:36 -0200

    Startup do Owin

---
+++ src/ByteBank.Forum/Startup.cs
@@ -0,0 +1,31 @@
+﻿using ByteBank.Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(ByteBank.Forum.Startup))]
+namespace ByteBank.Forum
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new AplicacaoDbContext());
+
+            app.CreatePerOwinContext<UserStore<UsuarioAplicacao>>((opt, cont) =>
+                new UserStore<UsuarioAplicacao>(
+                    cont.Get<AplicacaoDbContext>(null))
+            );
+
+            app.CreatePerOwinContext<UserManager<UsuarioAplicacao>>((opt, cont) =>
+                new UserManager<UsuarioAplicacao>(
+                    cont.Get<UserStore<UsuarioAplicacao>>(null))
+            );
+        }
+    }
+}

325a3200e0c827c216c1cceb8bb9230b8d4475f0
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-02-01 09:16:51 -0200

    Criação de ContaController, ViewModel de Registro e view

---
+++ src/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Registrar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        
+
+    }
+}
